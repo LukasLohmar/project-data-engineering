eb0e83ac31648ce080ad6cf301eda345611c658e
LukasLohmar <49276970+LukasLohmar@users.noreply.github.com>
2024-06-11 14:27:53 +0200

    create database abstraction in ef core and create migration scripts
    rename project root namespace

---
+++ src/data-system/Migrations/20240611121954_InitialMigration.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace DataSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SensorData",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TimeStamp = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    DeviceId = table.Column<PhysicalAddress>(type: "macaddr", nullable: false),
+                    CarbonDioxide = table.Column<decimal>(type: "numeric", nullable: false),
+                    Humidity = table.Column<decimal>(type: "numeric", nullable: false),
+                    Light = table.Column<bool>(type: "boolean", nullable: false),
+                    Lpg = table.Column<decimal>(type: "numeric", nullable: false),
+                    Motion = table.Column<bool>(type: "boolean", nullable: false),
+                    Smoke = table.Column<decimal>(type: "numeric", nullable: false),
+                    Temperature = table.Column<decimal>(type: "numeric", nullable: false),
+                    AdditionalData = table.Column<string>(type: "jsonb", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SensorData", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SensorData");
+        }
+    }
+}
---
+++ src/data-system/database/ApplicationContext.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Net.NetworkInformation;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataSystem.Database;
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<SensorData> SensorData { get; set; }
+
+    public ApplicationContext()
+    {
+        
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseNpgsql($"");
+}
+
+public class SensorData
+{
+    public int Id { get; set; }
+    [Column(TypeName = "timestamp without time zone")]
+    public DateTime TimeStamp { get; set; }
+    [Column(TypeName = "macaddr")]
+    public PhysicalAddress DeviceId { get; set; }
+    [Column(TypeName = "numeric")]
+    public decimal CarbonDioxide { get; set; }
+    [Column(TypeName = "numeric")]
+    public decimal Humidity { get; set; }
+    [Column(TypeName = "boolean")]
+    public bool Light { get; set; }
+    [Column(TypeName = "numeric")]
+    public decimal Lpg { get; set; }
+    [Column(TypeName = "boolean")]
+    public bool Motion { get; set; }
+    [Column(TypeName = "numeric")]
+    public decimal Smoke { get; set; }
+    [Column(TypeName = "numeric")]
+    public decimal Temperature { get; set; }
+    // for NOSQL-like data
+    [Column(TypeName = "jsonb")]
+    public string AdditionalData { get; set; }
+}
