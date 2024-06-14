bd780c62fa71d8a26abef6f5e4e39b6550bd72d6
LukasLohmar <49276970+LukasLohmar@users.noreply.github.com>
2024-06-14 15:50:27 +0200

    add controller test

--- src/DataSystem.Tests/Controller/DataControllerTest.cs
+++ src/DataSystem.Tests/Controller/DataControllerTest.cs
@@ -17,10 +17,11 @@ namespace DataSystem.Tests.Controller;
 public class DataControllerTest
 {
     private static readonly Mock<ApplicationContext> Context = new();
+    // ReSharper disable once InconsistentNaming
     private static DataController DataController;
 
     [ClassInitialize]
-    public static async Task ClassInitialize(TestContext testContext)
+    public static void ClassInitialize(TestContext testContext)
     {
         var authorization = new Authorization
         {
@@ -45,7 +46,7 @@ public class DataControllerTest
             new()
             {
                 Id = 2,
-                TimeStamp = DateTime.Now.AddHours(2),
+                TimeStamp = DateTime.Now.AddHours(1),
                 DeviceId = PhysicalAddress.Parse("A9612CF6BB21".AsSpan()),
                 Temperature = 30.3m,
                 ProviderToken = authorization
@@ -53,14 +54,14 @@ public class DataControllerTest
             new()
             {
                 Id = 3,
-                TimeStamp = DateTime.Now,
+                TimeStamp = DateTime.Now.AddHours(1),
                 DeviceId = PhysicalAddress.Parse("A9612CF6BB19".AsSpan()),
                 Temperature = 33.5m,
                 ProviderToken = authorization
             },
             new()
             {
-                Id = 3,
+                Id = 4,
                 TimeStamp = DateTime.Now.AddHours(2),
                 DeviceId = PhysicalAddress.Parse("A9612CF6BB21".AsSpan()),
                 Temperature = 28.4m,
@@ -98,5 +99,25 @@ public class DataControllerTest
         var result = await DataController.GetLatestData(token, deviceId, maxCount);
 
         Assert.AreEqual(expectedCount, result.Value?.Count);
+    }
+
+    [TestMethod]
+    [DataRow("11111111-1111-1111-1111-111111111112", 4, 1, 1, null, 0, 25, "ascending", "timestamp")]
+    [DataRow("11111111-1111-1111-1111-111111111112", 4, 4, 1, null, 0, 25, "descending", "timestamp")]
+    [DataRow("11111111-1111-1111-1111-111111111112", 2, 3, 2, null, 1, 2, "ascending", "timestamp")]
+    [DataRow("11111111-1111-1111-1111-111111111112", 4, 3, 1, null, 0, 5, "descending", "temperature")]
+    [DataRow("11111111-1111-1111-1111-111111111112", 2, 4, 1, "A9612CF6BB21", 0, 5, "ascending", "temperature")]
+    public async Task GetData(string token, int expectedCount, int expectedId, int totalPages, string deviceId, int pageIndex, int pageSize, string order, string orderValue)
+    {
+        var result = await DataController.GetData(token, deviceId, pageIndex, pageSize, order, orderValue);
+        
+        Assert.AreEqual(expectedCount, result.Value?.Items.Count);
+        Assert.AreEqual(pageIndex, result.Value?.PageIndex);
+        Assert.AreEqual(totalPages, result.Value?.TotalPages);
+        
+        if (expectedId != 0)
+        {
+            Assert.AreEqual(expectedId, result.Value?.Items.First().Id);
+        }
     }
 }
